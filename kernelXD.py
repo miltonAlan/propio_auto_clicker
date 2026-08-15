1b0f6c3dfb4acb26a0988e072a99a25eaf0dbf16
mpaucar <milton99o@gmail.com>
2026-08-15 18:30:39 -0500

    version mas solida del kernel

--- kernelXD.py
+++ kernelXD.py
@@ -1,10 +1,14 @@
+import subprocess
+import os
 import pyperclip
 import pyautogui
 import time
 from pynput import keyboard
 from datetime import datetime
 
-def checkAllPages():
+notas_file = "noutes.sql"
+
+def check_all_pages():
     pasos = [
         ("dead point click", (2679, 1066)),
         ("chrome", (200, 757)),
@@ -21,7 +25,7 @@ def checkAllPages():
                 log(f"Página {pagina}")
                 time.sleep(1) #HERE
 
-def VSCode():
+def load_VS_code():
     pasos = [
         ("3 lines", (29, 215)),
         ("archivo", (272, 224)),
@@ -36,7 +40,7 @@ def VSCode():
     pyperclip.copy('https://vscode.dev/')
     pyautogui.hotkey('ctrl', 'v')
     pyautogui.press('enter')
-    time.sleep(10)
+    time.sleep(20)
 
     for nombre, (x, y) in pasos:
         log(nombre)
@@ -44,29 +48,56 @@ def VSCode():
         time.sleep(2)
 
         if nombre == "nombre del archivo":
-            pyperclip.copy("propio.sql")
+            pyperclip.copy(str(notas_file))
             pyautogui.hotkey('ctrl', 'v')
             time.sleep(1)
 
     pyautogui.scroll(-1000)
     time.sleep(3)
-    # Blank space
+    # blank space click
     pyautogui.click(1156, 457)
-     
-    # Ctrl + Shift + Home
-    pyautogui.keyDown('ctrl')
-    pyautogui.keyDown('shift')
-    pyautogui.press('home')
-    
-    # Soltar Ctrl + Shift
-    pyautogui.keyUp('shift')
-    pyautogui.keyUp('ctrl')
-
-    
+    pyautogui.hotkey('enter')
+    # guardar cambios
+    time.sleep(2)
+    pyautogui.click(727, 188)
+        
+def create_noutes():
+    subprocess.run([
+        r"C:\Users\mpaucar\AppData\Local\Programs\Python\Python313\python.exe",
+        r"c:\Users\mpaucar\Desktop\propio_auto_clicker\notesCreator.py"
+    ])
+  
 def log(action):
     print(f"[{datetime.now().strftime('%H:%M:%S')}] {action}")
 
+def position_VS_code():
+    pasos = [
+        ("dead point click", (2679, 1066)),
+        ("pestana chrome", (300, 15)),
+    ]
+
+    for nombre, (x, y) in pasos:
+        pyautogui.moveTo(x, y)
+
+        if nombre == "pestana chrome":
+            pyautogui.mouseDown(button='left')
+
+            # mover SIN soltar el click
+            pyautogui.moveRel(925, 0, duration=0.5)
+            pyautogui.moveRel(0, 400, duration=0.5)
+
+            pyautogui.mouseUp(button='left')
+
+            print(f"Drag en {nombre} -> ({x}, {y})")
+
+        else:
+            pyautogui.click()
+            print(f"Click en {nombre} -> ({x}, {y})")
+
+        time.sleep(0.1)
 
 if __name__ == "__main__":
-    checkAllPages()
-    VSCode()
+    check_all_pages()
+    create_noutes()
+    load_VS_code()
+    
